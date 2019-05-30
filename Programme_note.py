42b3249bd958c51e25aab4b91ec0a96716d3ea04
hug0 <hd08029@gmail.com>
2019-05-30 15:07:53 +0200

    Update Programme_note.py

--- Programme_note.py
+++ Programme_note.py
@@ -7,37 +7,39 @@ import os
 os.chdir(path)
 print(os.getcwd())
 
-#FONCTIONS
+## Nous allons premierement créer toutes les fonctions dont nous allons avoir besoin dans la suite du programme
+
 def add(file): #fonction pour ajouter une nouvelle note
-    ca = 0
-    while ca != 2:
+    ca = 0   
+    while ca != 2: # la variable ca servira pour le menu
         ca = 0
-        name = input("Entrer le NOM de l'élève :")
+        name = input("Entrer le NOM de l'élève :") # entrez le nom de l'éleve
         name2 = input("Entrer le PRENOM de l'élève :")
-        note = "0"
-        checkp = -1
         whilecheck = 0
-        #permet de vérifier la validité de la note (nombre positif ou Abs)
-        while whilecheck == 0: 
+      
+        while whilecheck == 0:   # permet de vérifier la validité de la note (nombre positif ou Abs)
             try:
                 note = input("Entrer une note (positive et sur 20) ou Abs si l'élève était absent:")
                 if note != "Abs":
-                    note = float(note.replace(",",".")) 
+                    note = float(note.replace(",",".")) # remplace les virgules par des points pour eviter les erreurs
                     checkp = note
                     whilecheck = 1
-                if checkp < 0:
+                if checkp < 0:                             #  verifie si la note est positive
                     print("La note doit être positive!")
+                    whilecheck = 0
+                if checkp >= 20:                             #  verifie si la note est inférieure ou égale à 20
+                    print("La note doit être comprise entre 0 et 20!")
                     whilecheck = 0
                 if note == "Abs":
                     whilecheck = 1
-            except:
+            except:                                     # executer si la note est une chaine de caractères differente de Abs
                 print("La note n'est pas valide")
                 whilecheck = 0
-        #ouvre le fichier et écrit nom, prénom et note
-        f = open(file +".txt",'a')
+        
+        f = open(file +".txt",'a') #ouvre le fichier et écrit nom, prénom et note
         f.write(str(note) + " " + name + " " + name2 + "\n")
         f.close
-        #demande si on veut saisir une autre note. Si oui on retourne au while du début sinon on quitte la boucle
+        #demande dans la foulée si on veut saisir une autre note. Si oui on retourne au while du début sinon on quitte la boucle
         while ca not in [1,2]:
             print("Souhaitez-vous saisir une autre note ?:\n 1.Oui\n 2.Non")
             try:
@@ -54,7 +56,7 @@ def ask(): #fonction pour demander le nom du devoir. Si on rentre "Annuler", le 
     askn = 0
     while askn == 0:
         file = input("Nom du devoir:")
-        if os.path.isfile(path + file + ".txt"):
+        if os.path.isfile(path + file + ".txt"): # verifie si le fichier existe, s'il existe : os.path.isfile(path + file + ".txt") prend la valeur true
             return file
             askn = 1
         elif file == "Annuler":
@@ -67,8 +69,8 @@ def getNote(file, nbligne):
     i = 0
     f=open(file + ".txt",'r')
     for i in range(1, nbligne):
-        note = f.readline()
-        note = note.split()
+        ligne = f.readline()
+        note = ligne.split()
     f.close
     return note[0]
         
@@ -82,13 +84,13 @@ def getName(file, nbligne):
     f.close
     return name[1]
     
-def getLignFromName(file, name):
+#def getLignFromName(file, name):
     
-def lignCount(file):   
+def lignCount(file):   # fonction qui sert a compter le nombre de ligne
     i = 0
     f=open(file + ".txt",'r')
     for x in f:
-        i += 1
+        i += 1   #i = i + 1
     f.close
     return i
     
@@ -117,7 +119,8 @@ def redir():
     import os
     os.chdir(path)
 
-#PROGRAMME
+## DEBUT DU PROGRAMME
+
 while c != 6: #Tant que c est différent on reste dans le programme sinon si c=6 ==> "Quitter" donc on sort du programme
     print("MENU:\n 1.Nouveau devoir\n 2.Consulter des notes\n 3.Ajouter/Modifier des notes\n 4.Obtenir une moyenne\n 5.Supprimer un devoir\n 6.Quitter\n")
     try:
@@ -130,15 +133,14 @@ while c != 6: #Tant que c est différent on reste dans le programme sinon si c=6
     #1 NOUVEAU DEVOIR
     if c == 1:
         c == 0
-        L = []
         redir()
-        file = input("Saisissez le nom du devoir:")
-        if os.path.isfile(path + file + ".txt"):
+        fichier = input("Saisissez le nom du devoir ou Annuler pour revenir au menu précédent:")
+        if os.path.isfile(path + fichier + ".txt"):
             print("Le devoir existe déjà !")
-        elif file != "Annuler":
-            new(file)
+        elif fichier != "Annuler":
+            new(fichier)
             n = 0
-            while n not in [1,2]:
+            while n not in [1,2]:  # donne la possibilité de créer un fichier mais de rentrer les notes plus tard
                 print("Souhaitez-vous saisir des notes ?:\n 1.Oui\n 2.Non")
                 try:
                     n = int(input("Que souhaitez-vous faire ? (Saisissez le numéro correspondant à l'action de votre choix):"))
@@ -147,7 +149,7 @@ while c != 6: #Tant que c est différent on reste dans le programme sinon si c=6
                 except:
                     print("Veuillez choisir 1 ou 2!")
                 if n == 1:
-                    add(file)
+                    add(fichier)
         
     #2 CONSULTER
     if c == 2:
@@ -187,15 +189,14 @@ while c != 6: #Tant que c est différent on reste dans le programme sinon si c=6
                 print("Veuillez choisir 1, 2 ou 3!")
             if ce == 1:
                 print("Ajouter une note à un devoir. Saisissez le nom du devoir ou \"Annuler\" pour sortir de ce menu")
-                fileA = ask()
+                fileA = ask()        # demande le nom du fichier
                 if fileA != "Annuler":
-                    filesA = ask()
-                    add(filesA)
+                    add(fileA)
                 ce = 3
             if ce == 2:
                 print("Modifier une note d'un devoir. Saisissez le nom du devoir ou \"Annuler\" pour sortir de ce menu")
                 fileU = ask()
-                if fileU != "Annuler":
+                if fileU != "Annuler": 
                     filesU = ask()
                     name = askN()
                     edit(filesU)
@@ -203,7 +204,27 @@ while c != 6: #Tant que c est différent on reste dans le programme sinon si c=6
         
     #4 MOYENNE
     if c == 4:
-        print("test")
+        while ce!= 2:
+            print("Souhaitez-vous consulter la moyenne d'un devoir ?:\n 1.Devoir\n 2.Annuler")
+            try:
+                ce = int(input("Que souhaitez-vous faire ? (Saisissez le numéro correspondant à l'action de votre choix):"))
+                if ce not in [1,2]:
+                    print("Veuillez choisir 1 ou 2!")
+            except:
+                print("Veuillez choisir 1 ou 2!")
+            if ce == 1:
+                print("Consulter la moyenne d'un devoir. Saisissez le nom du devoir ou \"Annuler\" pour sortir de ce menu")
+                fichier = ask()        # demande le nom du fichier
+                if fichier != "Annuler":
+                    nbligne = lignCount(fichier)
+                    total = 0
+                    numberline = 0
+                    for numberline in range (1, nbligne):
+                        note2 = getNote(fichier, numberline)
+                        total += float(note)
+                    moyenne = total / nbligne
+                    print(moyenne)
+                ce = 2
            
     #5 SUPPRIMER
     if c == 5:
